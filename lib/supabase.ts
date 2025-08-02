4b3970915915f6f56c295cf3b68bbfe41780f133
Nicolas Bertorello <125308763+nicob404@users.noreply.github.com>
2025-08-01 22:11:09 -0300

    Update supabase.ts

--- lib/supabase.ts
+++ lib/supabase.ts
@@ -1,21 +1,6 @@
-import { createBrowserClient } from '@supabase/auth-helpers-nextjs'
-import { createServerComponentClient } from '@supabase/auth-helpers-nextjs'
-import { createServerActionClient } from '@supabase/auth-helpers-nextjs'
-import { type CookieOptions } from '@supabase/auth-helpers-shared'
-import { type Database } from '@/types/supabase'
+import { createClient } from '@supabase/supabase-js'
 
-export const supabase = createBrowserClient<Database>(
-  process.env.NEXT_PUBLIC_SUPABASE_URL!,
-  process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
-)
+const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL!
+const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
 
-export const createClientComponentClient = () => createBrowserClient<Database>(
-  process.env.NEXT_PUBLIC_SUPABASE_URL!,
-  process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
-)
-
-export const createServerClient = (cookies: () => CookieOptions) =>
-  createServerComponentClient<Database>({ cookies })
-
-export const createActionClient = (cookies: () => CookieOptions) =>
-  createServerActionClient<Database>({ cookies })
+export const supabase = createClient(supabaseUrl, supabaseAnonKey)
